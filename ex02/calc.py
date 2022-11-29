6cc9bb1808dc8da05858aa3310426416df2a65f3
Yuto Ishikawa <c0a21013ef@edu.teu.ac.jp>
2022-11-29 18:28:42 +0900

    改良版 #8

--- ex02/calc.py
+++ ex02/calc.py
@@ -5,6 +5,7 @@ import tkinter.messagebox as tkm
 def button_click(event):
     btn = event.widget
     n = btn["text"]
+
     if n == "=":
         ikolove = entry.get()      # 数式の文字列
         res = eval(ikolove)        # 数式文字列の評価
@@ -18,7 +19,7 @@ def button_click(event):
 
 
 root = tk.Tk()
-root.geometry("300x630")
+root.geometry("400x650")
 
 entry = tk.Entry(root, justify="right", width=10, font=("",40))
 entry.grid(row = 0, column=0, columnspan=3) 
@@ -33,15 +34,25 @@ for n in range(9, -1, -1):
         r += 1
         c = 0
 
-operators = ["+","-","/","*","="]  #四則演算
+operators = ["+","-","/","*"]  #四則演算
+iko = ["="]
+r = 1
 for ope in operators:
     button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
-    button.grid(row=r, column=c)
+    button.grid(row=r, column=3)
     button.bind("<1>", button_click)
-    c += 1
-    if c%3 == 0:
-        r += 1
-        c = 0
+    r += 1
+
+for ope in iko:
+    button = tk.Button(root, text=f"{ope}", width=4, height=2, font=("", 30))
+    button.grid(row=4, column=2)
+    button.bind("<1>", button_click)
+    r += 1
+
+    #c += 1
+    #if c%3 == 0:
+        #r += 1
+        #c = 0
 
 root.mainloop()
 
