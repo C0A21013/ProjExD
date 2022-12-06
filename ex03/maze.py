79bb4accf85f39fe7ee6d3fefdba7c0757ec433d
Yuto Ishikawa <c0a21013ef@edu.teu.ac.jp>
2022-12-06 17:19:51 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -12,7 +12,7 @@ def key_up(event):
 
 
 def main_proc():
-    global cx, cy, mx, my
+    global cx, cy, mx, my,tori,tori2,canvas
     if key == "Up": my -= 1
     if key == "Down": my += 1
     if key == "Left": mx -= 1
@@ -21,7 +21,14 @@ def main_proc():
         if key == "Up": my += 1
         if key == "Down": my -= 1
         if key == "Left": mx += 1
-        if key == "Right": mx -= 1        
+        if key == "Right": mx -= 1
+        canvas.create_image(cx, cy, image=tori2, tag="kokaton") 
+
+
+
+        
+    
+
     cx, cy = mx*100+50, my*100+50
     canvas.coords("kokaton", cx, cy)
     root.after(100, main_proc)
@@ -29,6 +36,8 @@ def main_proc():
 
 if __name__ == "__main__":
     root = tk.Tk()
+    tori2 = tk.PhotoImage(file="fig/8.png")
+
     root.title("迷えるこうかとん")
     canvas = tk.Canvas(root, width=1500, height=900, bg="black")
     canvas.pack()
@@ -39,8 +48,10 @@ if __name__ == "__main__":
 
     mx, my = 1, 1
     cx, cy = mx*100+50, my*100+50
-    tori = tk.PhotoImage(file="fig/8.png")
+    tori = tk.PhotoImage(file="fig/9.png")
     canvas.create_image(cx, cy, image=tori, tag="kokaton")
+    canvas.pack()
+
     key = ""
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
